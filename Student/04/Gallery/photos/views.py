ace5701303426bd0db19d0c2da3d5a4d8aa0a900
fierroe297 <fier9958@bears.unco.edu>
2024-09-22 00:53:49 -0600

    Updated to have buttons for each hero to swap pages

--- Student/04/Gallery/photos/views.py
+++ Student/04/Gallery/photos/views.py
@@ -1,16 +1,40 @@
 from pathlib import Path
+from typing import Any
 from django.views.generic import TemplateView
 
 
 def photo_list():
     def photo_details(i, f):
-        caption = f'Caption for Photo {i}' if i == 1 else None
+        caption = f'Caption for Photo {i}' 
+        if i == 0:
+            caption = f'BeastBoy: He can transform into any animal but is always green.'
+        if i == 1:
+            caption = f'Cyborg: Has all the best technology, but would not reccomend getting wet'
+        if i == 2:
+            caption = f'Raven: Magical abilites with wild power but has daddy issues'
+        if i == 3: 
+            caption = f'Robin: Seasoned acrobat with plenty of training, but he has no powers'
+        if i == 4:
+            caption = f'Starfire: Super strong alien with lazers, but very gulible'
         return dict(id=i, file=f, caption=caption)
 
     photos = Path('static/images').iterdir()
     photos = [photo_details(i, f) for i, f in enumerate(photos)]
     return photos
 
+def hero_list(i, f):
+    if i == 0:
+        name = f'BeastBoy'
+    if i == 1:
+        name = f'Cyborg'
+    if i == 2:
+        name = f'Raven'
+    if i == 3:
+        name = f'Robin'
+    if i == 4:
+        name = f'Starfire'
+    return dict(id=i, file=f, name=name)
+    
 
 class PhotoListView(TemplateView):
     template_name = 'photos.html'
@@ -27,3 +51,8 @@ class PhotoDetailView(TemplateView):
         photos = photo_list()
         p = photos[i]
         return dict(photo=p)
+
+class DetailListView(TemplateView):
+    template_name = 'list.html'
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        return super().get_context_data(**kwargs)
