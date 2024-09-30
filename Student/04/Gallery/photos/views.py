89570afdbf2684f749aa7fd60e80acb403ae9742
fierroe297 <fier9958@bears.unco.edu>
2024-09-29 20:57:45 -0600

    Created buttons to have names of heros

--- Student/04/Gallery/photos/views.py
+++ Student/04/Gallery/photos/views.py
@@ -6,35 +6,27 @@ from django.views.generic import TemplateView
 def photo_list():
     def photo_details(i, f):
         caption = f'Caption for Photo {i}' 
+        name = f'{i}'
         if i == 0:
             caption = f'BeastBoy: He can transform into any animal but is always green.'
+            name = f'BeastBoy'
         if i == 1:
             caption = f'Cyborg: Has all the best technology, but would not reccomend getting wet'
+            name = f'Cyborg'
         if i == 2:
             caption = f'Raven: Magical abilites with wild power but has daddy issues'
+            name = f'Raven'
         if i == 3: 
             caption = f'Robin: Seasoned acrobat with plenty of training, but he has no powers'
+            name = f'Robnin'
         if i == 4:
             caption = f'Starfire: Super strong alien with lazers, but very gulible'
-        return dict(id=i, file=f, caption=caption)
+            name = 'starfire'
+        return dict(id=i, file=f, caption=caption, name=name)
 
     photos = Path('static/images').iterdir()
     photos = [photo_details(i, f) for i, f in enumerate(photos)]
-    return photos
-
-def hero_list(i, f):
-    if i == 0:
-        name = f'BeastBoy'
-    if i == 1:
-        name = f'Cyborg'
-    if i == 2:
-        name = f'Raven'
-    if i == 3:
-        name = f'Robin'
-    if i == 4:
-        name = f'Starfire'
-    return dict(id=i, file=f, name=name)
-    
+    return photos    
 
 class PhotoListView(TemplateView):
     template_name = 'photos.html'
